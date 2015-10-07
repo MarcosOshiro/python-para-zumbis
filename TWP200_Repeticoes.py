34899567d3a9f08fa12e5ae9da50d856522ea444
Marcos Katsuhico Oshiro <mkobuzz@gmail.com>
2015-10-07 20:43:50 -0300

    Exercicio de videos

---
+++ TWP200_Repeticoes.py
@@ -0,0 +1,7 @@
+n_user=int(input('Entre com o número: '))
+
+x = 1
+
+while x <= n_user:
+    print(x)
+    x = x +1
---
+++ TWP210_Inteiros_ate_um_fim.py
@@ -0,0 +1,8 @@
+n_user=int(input('Entre com o número: '))
+
+x = 0
+
+while x <= n_user:
+    if x % 2 == 0:
+        print(x)
+    x = x +1
