efb3f71ae3d355c8f965097beb80c1035c321e10
Marcos Katsuhico Oshiro <mkobuzz@gmail.com>
2015-10-08 22:27:34 -0300

    Exercício feito twp273

--- TWP273_Vetor3.py
+++ TWP273_Vetor3.py
@@ -1,35 +1,14 @@
 '''
 Faça um programa que leia 4 notas, mostre as notas e a média na tela
 '''
-
-
-
-#Daqui para baixo código do exercício anterior
-vetor = []
-x = 0
-while x < 10:
-    add = float(input("Digite a posição %d: " %x))
+notas=[]
+x = 1
+soma = 0
+while x <= 4:
+    add = float(input("Nota %d: " %x))
     x += 1
-    vetor.append(add)
-if x == 10:
-    while x <= 10:
-        x -= 1
-        if x < 0:
-            break
-        else:
-            print(vetor[x])
-
-'''
-Como foi feito no video
-
-vetor = []
-i = 1
-while i <= 10:
-    n = float(input("Digite um número: "))
-    vetor.append(n)
-    i += 1
-i = 9
-while i >= 0:
-    print (vetor[9])
-    i -= 1
-'''
+    notas.append(add)
+    soma += add
+print(notas)
+print("------")
+print("%2.2f" %(soma/4))
