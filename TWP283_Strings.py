f6e97ab6ed761695ce5d427abf9006102c597a59
unknown <moshiro@DTEMOSHIRO.dte.local>
2015-11-05 15:20:59 -0200

    inicio de solução de exercício

--- TWP283_Strings.py
+++ TWP283_Strings.py
@@ -2,3 +2,17 @@
 Faça um programa que leia uma palavra e troque as vogais por "*"
 '''
 
+entrada = input("Entre com a palavra: ")
+vogais=["a","e","i","o","u"]
+
+contvogal = 0
+x = 1
+
+while x <= 10:
+
+    x += 1
+    caract.append(entrada)
+    if entrada in vogais:
+        contvogal += 1
+print ("Consoantes: ",(len(caract))-contvogal)
+
