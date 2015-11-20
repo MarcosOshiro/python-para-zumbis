fa4fac1e122008dfa28eba70286e203e84d33020
Marcos Katsuhico Oshiro <mkobuzz@gmail.com>
2015-11-19 22:45:28 -0200

    fiz um jeito mais ou menos diferente do exercicio colado descaradamente

--- Lista 3/questao04.py
+++ Lista 3/questao04.py
@@ -5,7 +5,7 @@ de então, cada elemento é a soma dos dois anteriores. Faça um algoritmo que
 leia um número inteiro calcule o seu número de Fibonacci. F1 = 1, F2 = 1,
 F3 = 2, etc.
 '''
-
+'''
 def fib(n):
  a,b = 1,1
  for i in range(n-1):
@@ -13,6 +13,17 @@ def fib(n):
  return a
 print (fib(6))
 
-'''
+
 Copiei descaradamente, não consegui aplicar conhecimento aqui. Preocupante.
+Abaixo só consegui pq entendi o que o maluco de cima fez... too sad ):
 '''
+
+n = int(input("Número: "))
+a = 1
+b = 1
+x = 1
+while x <= (n-1): #n-1 pq começa do zero
+    a,b = b, a+b
+    x += 1
+
+print (a)
