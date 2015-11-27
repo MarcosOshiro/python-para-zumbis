13a83d8a91c07ac7edb68f24141be404ca14b01a
Marcos Katsuhico Oshiro <mkobuzz@gmail.com>
2015-11-27 21:38:07 -0200

    Feito exercício 1 da lista de desafios.

--- Lista 3 Desafios/questao01.py
+++ Lista 3 Desafios/questao01.py
@@ -1,10 +1,24 @@
 '''
 Dizemos que um número natural é triangular se ele é produto de três números
-naturais consecutivos.  Exemplo: 120 é triangular, pos 4.5.6 = 120.  Dado um
+naturais consecutivos.  Exemplo: 120 é triangular, pois 4.5.6 = 120.  Dado um
 inteiro não-negativo n, verificar se n é triangular.
 '''
 
+n = int(input("Entre com o número: "))
 
+a, b, c = 1, 2, 3
 
+total = 0
+while total <= n:
+    if total == n:
+        print("Este é um número triangular")
+        break
+    else:
+        total = a * b * c
+        a += 1
+        b += 1
+        c += 1
+if total > n:
+    print("Este não é um número triangular")
 
 
