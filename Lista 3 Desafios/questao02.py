8f937ebef80932f6d4565675ca099cdc917f56ed
Marcos Katsuhico Oshiro <mkobuzz@gmail.com>
2015-11-27 23:40:27 -0200

    Feito exercício 2 da lista de desafio, ficou bem feio, apesar de funcional. pensar em como melhorar a lógica e o código

--- Lista 3 Desafios/questao02.py
+++ Lista 3 Desafios/questao02.py
@@ -5,6 +5,70 @@ efetuado desprezando os centavos.  Suponha que as notas para troco sejam as de
 50, 20, 10, 5, 2 e 1 reais, e que nenhuma delas esteja em falta no caixa.
 '''
 
+conta = float(input("Entre com o total da conta: "))
+pagamento = float(input("Entre com o pagamento: "))
+troco = pagamento - conta
+n50 = 0 
+n20 = 0
+n10 = 0
+n5 = 0
+n2 = 0
+n1 = 0
+sum_notas = n50 + n20 + n10 + n5 + n2 + n1
+calc = troco
+
+while calc <= troco:
+    if calc <= 1.0:
+        break
+    elif calc >= 50.0:
+        calc = calc - 50.0
+        n50 += 1
+    elif calc < 50.0:
+        if calc <= 1.0:
+            break
+        elif calc >= 20.0:
+            calc = calc - 20.0
+            n20 += 1
+        elif calc < 20.0:
+            if calc <= 1.0:
+                break
+            elif calc >= 10.0:
+                calc = calc - 10.0
+                n10 += 1
+            elif calc < 10.0:
+                if calc <= 1.0:
+                    break
+                elif calc >= 5.0:
+                    calc = calc - 5.0
+                    n5 += 1
+                elif calc < 5.0:
+                    if calc <= 1.0:
+                        break
+                    elif calc >= 2.0:
+                        calc = calc - 2.0
+                        n2 += 1
+                    elif calc < 2.0:
+                        if calc <= 1.0:
+                            break
+                        elif calc >= 1.0:
+                            calc = calc - 1.0
+                            n1 += 1
+                        elif calc < 1.0:
+                            if calc <= 1.0:
+                                break
+print("Foram usadas %d notas" %sum_notas)
+print("Qtde de R$50,00: %d" %n50)            
+print("Qtde de R$20,00: %d" %n20)
+print("Qtde de R$10,00: %d" %n10)
+print("Qtde de R$5,00: %d" %n5)
+print("Qtde de R$2,00: %d" %n2)
+print("Qtde de R$1,00: %d" %n1)
+
+    
+        
+
+
+
 
 
 
