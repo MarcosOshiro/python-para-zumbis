72f06cd74b5f35c0749d768f5585e917937f2e00
Marcos Katsuhico Oshiro <mkobuzz@gmail.com>
2015-12-03 08:56:03 -0200

    lógica diferente para o exercicio 2 do desafio 3

--- Lista 3 Desafios/questao02.py
+++ Lista 3 Desafios/questao02.py
@@ -5,6 +5,8 @@ efetuado desprezando os centavos.  Suponha que as notas para troco sejam as de
 50, 20, 10, 5, 2 e 1 reais, e que nenhuma delas esteja em falta no caixa.
 '''
 
+'''Primeira tentativa
+
 conta = float(input("Entre com o total da conta: "))
 pagamento = float(input("Entre com o pagamento: "))
 troco = pagamento - conta
@@ -63,7 +65,25 @@ print("Qtde de R$10,00: %d" %n10)
 print("Qtde de R$5,00: %d" %n5)
 print("Qtde de R$2,00: %d" %n2)
 print("Qtde de R$1,00: %d" %n1)
+'''
 
+conta = int(input("Digite o valor total da compra: "))
+pagamento = int(input("Digite a quantia de dinheiro recebida: "))
+
+
+troco = pagamento-conta
+notas = [100, 50, 20, 10, 5, 2, 1]
+
+if troco > 0:
+    print ("Valor do troco: R$ %s." %troco)
+    for p in notas:
+        if troco >= p:
+            n = int(troco/p)
+            r = troco - p*n
+            print (" %s nota(s) de R$ %s." %(n, p))
+            troco = r
+else:
+    print ("Pagamento insuficiente.")
     
         
 
