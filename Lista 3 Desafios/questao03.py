9a215077982fae0d24efcd88e72ccdae83b79de9
Marcos Katsuhico Oshiro <mkobuzz@gmail.com>
2015-12-03 09:07:25 -0200

    Exercício 3 da lista 3 de desafios.  Funcional e sem erro de expressão booleana que deu qdo coloquei tudo no mesmo if

--- Lista 3 Desafios/questao03.py
+++ Lista 3 Desafios/questao03.py
@@ -2,7 +2,14 @@
 Verifique se um inteiro positivo n é primo.
 '''
 
+n = int(input("Entre com um número inteiro positivo: "))
 
+if n%2 == 0:
+    print("Não é primo")
+elif n%3 == 0:
+    print("Não é primo")
+else:
+    print("Número primo")
 
 
 
