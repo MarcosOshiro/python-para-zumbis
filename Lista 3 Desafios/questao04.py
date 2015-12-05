b6f57960b085cd4ce4fcbbea64fcd2a4f01ca5c3
Marcos Katsuhico Oshiro <mkobuzz@gmail.com>
2015-12-04 23:44:38 -0200

    Lista de desafio 3... questões 4 e 5

--- Lista 3 Desafios/questao04.py
+++ Lista 3 Desafios/questao04.py
@@ -5,5 +5,15 @@ calculando também a multiplicidade de cada fator.
 
 
 
+n = int(input('Entre com o número: '))
 
+divisores = []
+d = 2
+while n > 1:
+    if n%d == 0:
+        n = n/d
+        divisores.append(d)
+    else:
+        d += 1
+print ("Estes são os divisores", divisores)
 
--- Lista 3 Desafios/questao05.py
+++ Lista 3 Desafios/questao05.py
@@ -3,7 +3,11 @@ Faça um programa que peça um inteiro positivo e o mostre invertido. Ex:
 1234 gera 4321
 '''
 
+n = int(input("Entre com o número: "))
 
+inv = int(str(n)[::-1])
+print (inv)
 
+#Fiquei um tempo preso em len de int dando erro... até pensar em converter a variável... :(
 
 
