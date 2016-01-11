921cf59c36aed55ac12ef02b52e13a8ac4770c97
unknown <moshiro@DTEMOSHIRO.dte.local>
2016-01-11 12:35:44 -0200

    Voltando para as aulas. Parei no pingmind na twp292 Funçoes, tem exercicios la para fazer

---
+++ TWP290_ForEhWhileEnrustido.py
@@ -0,0 +1,39 @@
+'''
+texto = "aeiou"
+k = 0
+while k < len(texto):
+    letra = texto[k]
+    print (letra)
+    k = k + 1
+-----------------------
+
+for letra in "aeiou":
+    print (letra)
+'''
+
+'''
+lista = list(range(5))
+k = 0
+while k < len(lista):
+    i = lista[k]
+    print(i)
+    k = k + 1
+------------------------
+
+for i in range(5):
+    print(i)
+'''
+
+'''
+lista = ['cpbr6', 42, 3.14]
+k = 0
+while k < len(lista):
+    x = lista[k]
+    print(x)
+    k = k + 1
+------------------------
+
+for x in['cpbr6', 42, 3.14]:
+    print(x)
+'''
+
---
+++ TWP292_Funcoes.py
@@ -0,0 +1,3 @@
+def epar(x):
+    return x%2 == 0
+
