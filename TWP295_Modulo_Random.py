c89d3f75cf5cbf00d8efc32f3af6408b55bb1bfe
Marcos Katsuhico Oshiro <mkobuzz@gmail.com>
2016-02-01 22:44:56 -0200

    feito exercicios, continuar do 297... precisa terminar

---
+++ TWP295_Modulo_Random.py
@@ -0,0 +1,22 @@
+def embaralha(s):
+    import random
+    lista = list(s)
+    random.shuffle(lista)
+    return ''.join(lista)
+
+
+
+
+
+''' Como fiz
+
+a = input('Entre com a string: ')
+import random
+def embaralha(a):
+    b = list(a)
+    random.shuffle(b)
+    return b
+
+
+print(embaralha(a))
+'''
---
+++ TWP296_Embaralha.py
@@ -0,0 +1,15 @@
+import random
+
+i = 0
+while i < 15:
+    print(random.randint(10, 100))
+    i += 1
+    
+''' Como foi feito no video
+
+import random
+lista = []
+for k in range(15):
+    lista.append(ramdom.randint(10, 100))
+print (lista)
+'''
