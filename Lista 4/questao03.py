88639d6ca6e4dc41c4760ff23b0a41265a116855
Marcos Katsuhico Oshiro <mkobuzz@gmail.com>
2016-03-23 22:34:34 -0300

    Precisa fazer o exercicio 4, só criei o arquivo com o enunciado.  O exercício 3 está feito, talvez não da forma ideal, mas está funcional..

--- Lista 4/questao03.py
+++ Lista 4/questao03.py
@@ -1,25 +1,33 @@
 '''
-2. Sorteie 20 inteiros entre 1 e 100 numa lista.
-Armazene os números pares na lista PARe os números ímpares na lista IMPAR.
-Imprima as três listas.
+3. Faça um programa que crie dois vetores com 10 elementos aleatórios entre 1
+e 100. Gere um terceiro vetor de 20 elementos, cujos valores deverão ser
+compostos pelos elementos intercalados dos dois outros vetores.
+Imprima os três vetores.
 '''
 import random
-lista = []
-listapar = []
-listaimpar = []
+lista1 = []
+lista2 = []
+lista3 = []
 
-for x in range(20):
+while len(lista1) < 10:
     a = random.randint(1, 100)
-    if a not in lista:
-        lista.append(a)
-    if a %2 ==0:
-        listapar.append(a)
+    if a not in lista1:
+        lista1.append(a)
+while len(lista2) < 10:
+    a = random.randint(1, 100)
+    if a not in lista1:
+        lista2.append(a)
+def epar(x):
+    return x%2 == 0
+lista1.sort()
+lista2.sort()
+lista3.sort()
+for x in range(10):
+    if epar(x) == True:
+        lista3.append(lista2[x])
     else:
-        listaimpar.append(a)
-    
-lista.sort()
-listapar.sort()
-listaimpar.sort()
-print (lista)
-print (listapar)
-print (listaimpar)
+        lista3.append(lista1[x])
+
+print (lista1)
+print (lista2)
+print (lista3)
---
+++ Lista 4/questao04.py
@@ -0,0 +1,12 @@
+'''
+4. Seja o statement sobre diversidade: “The Python Software Foundation and
+the global Python community welcome and encourage participation by everyone.
+Our community is based on mutual respect, tolerance, and encouragement,
+and we are working to help each other live up to these principles. We want
+our community to be more diverse: whoever you are, and whatever your background,
+we welcome you.”. Gere uma lista de palavras deste texto com split(), a seguir
+crie uma lista com as palavras que começam ou terminam com uma das letras
+“python”. Imprima a lista resultante. Não se esqueça de remover antes os
+caracteres especiais e cuidado com maiúsculas e minúsculas. 
+'''
+
